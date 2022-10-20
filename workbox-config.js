44b9e7e85d3335882bb648f2ba602d45988d4632
Barrera12373 <arce.12373@gmail.com>
2022-10-20 12:24:00 -0500

    3

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	globDirectory: '.',
 	globPatterns: [
-		'**/*.{png,html,json}'
+		'**/*.{png,css,eot,svg,ttf,woff,woff2,otf,txt,html,jpg,js,json,gif,rb,less,scss,mp4}'
 	],
 	swDest: 'sw.js',
 	ignoreURLParametersMatching: [
